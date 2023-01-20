5ab23ab10ee139a92eddf651dda59815df10c311
gabd-el- <gabd-el-@student.42wolfsburg.de>
2023-01-20 22:26:53 +0100

    added files

---
+++ ft_substr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabd-el- <gabd-el-@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/18 18:01:43 by codespace         #+#    #+#             */
+/*   Updated: 2023/01/17 21:59:24 by gabd-el-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char			*sub;
+	unsigned int	i;
+
+	i = 0;
+	if (start > ft_strlen((char *)s))
+	{
+		sub = (char *) malloc(sizeof(char) * (1));
+		if (sub == NULL)
+			return (NULL);
+		sub[i] = '\0';
+		return (sub);
+	}
+	sub = (char *)malloc(sizeof(char) * (len + 1));
+	if (sub == NULL)
+		return (NULL);
+	while (i < len)
+	{
+		sub[i] = s[start + i];
+		i++;
+	}
+	sub[i] = '\0';
+	return (sub);
+}
